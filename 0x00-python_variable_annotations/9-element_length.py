d97d23d5ce86f2dc5f9c99a5c884c0e4a43b1041
hunterxx0 <azouzmhamed@gmail.com>
2021-03-15 14:49:05 +0100

    first

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -2,6 +2,7 @@
 """ make_multiplier module """
 import typing
 
+
 def element_length(lst: typing.Iterable[typing.Sequence]) -> typing.List[typing.Tuple[typing.Sequence, int]]:
     """ element_length module """
     return [(i, len(i)) for i in lst]
