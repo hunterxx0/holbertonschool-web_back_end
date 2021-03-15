a2e9f2fb9da45e09c432819cd35d4aec7a13ede3
hunterxx0 <azouzmhamed@gmail.com>
2021-03-15 13:06:46 +0100

    first

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" sum_mixed_list module """
+import typing
+
+
+def sum_mixed_list(mxd_lst: typing.List[typing.Union[int, float]]) -> float:
+    """ sum_mixed_list module """
+    return (sum(mxd_lst))
