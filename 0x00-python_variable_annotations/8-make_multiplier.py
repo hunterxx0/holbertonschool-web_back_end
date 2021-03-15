1a529f93a3c215f4fe02e4f47ada46ba3880a3ec
hunterxx0 <azouzmhamed@gmail.com>
2021-03-15 14:06:26 +0100

    first

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -10,4 +10,7 @@ def mult(x: float) -> float:
 
 def make_multiplier(multiplier: float) -> typing.Callable[[float], float]:
     """ make_multiplier module """
+    def mult(x: float) -> float:
+	    """ make_multiplier module """
+	    return (multiplier * x)
     return (mult)
