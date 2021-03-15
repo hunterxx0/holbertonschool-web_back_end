6a990e4e26779f942902ca185864f36ed9dd4fc8
hunterxx0 <azouzmhamed@gmail.com>
2021-03-15 14:50:30 +0100

    first

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -3,6 +3,7 @@
 import typing
 
 
-def element_length(lst: typing.Iterable[typing.Sequence]) -> typing.List[typing.Tuple[typing.Sequence, int]]:
+def element_length(lst: typing.Iterable[typing.Sequence]) ->
+typing.List[typing.Tuple[typing.Sequence, int]]:
     """ element_length module """
     return [(i, len(i)) for i in lst]
