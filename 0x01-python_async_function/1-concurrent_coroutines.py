7f199d5674b14e4b186a2648b4ed0b1ec156a232
hunterxx0 <azouzmhamed@gmail.com>
2021-03-16 19:29:40 +0100

    first

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" wait_random module """
+import asyncio
+import random
+import typing
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int = 10) -> typing.List[float]:
+    """ wait_random module """
+    return [await wait_random(max_delay) for i in range(n)]
