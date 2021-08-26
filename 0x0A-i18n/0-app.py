d4b03f066c7d45eb36e1fb2045decd596639be88
hunterxx0 <azouzmhamed@gmail.com>
2021-08-26 13:37:17 +0100

    first

--- 0x0A-i18n/0-app.py
+++ 0x0A-i18n/0-app.py
@@ -21,4 +21,6 @@ def root() -> str:
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000")
+    host = getenv("API_HOST", "0.0.0.0")
+    port = getenv("API_PORT", "5000")
+    app.run(host=host, port=port)
