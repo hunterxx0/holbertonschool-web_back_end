be45f321d1c4210b7e524a33ece762ac7a1e89de
hunterxx0 <azouzmhamed@gmail.com>
2021-08-26 13:31:45 +0100

    first

---
+++ 0x0A-i18n/0-app.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""
+FLask module
+"""
+from flask import Flask, jsonify, request, abort, redirect
+from flask_babel import Babel
+
+app = Flask(__name__)
+babel = Babel(app)
+app.config['LANGUAGES'] = ["en", "fr"]
+app.config['BABEL_DEFAULT_LOCALE'] = 'en'
+app.config['BABEL_DEFAULT_TIMEZONE'] = 'UTC'
+
+
+@app.route('/')
+def root() -> str:
+    """
+    root route
+
+    Returns:
+
+    a Welcome message
+    """
+    return render_template('0-index.html')
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
