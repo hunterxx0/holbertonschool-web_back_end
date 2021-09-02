cd62835f435ed633eb5abe0c0e699d8da750c3bb
hunterxx0 <azouzmhamed@gmail.com>
2021-09-02 21:02:12 +0100

    first

--- 0x0C-MySQL_Advanced/3-glam_rock.sql
+++ 0x0C-MySQL_Advanced/3-glam_rock.sql
@@ -1,9 +1,6 @@
 -- Old school band
-SELECT * 
-FROM (
-	SELECT band_name,
-	(IFNULL(split, 2021) - formed) as lifespan
-	FROM metal_bands
-	WHERE style LIKE "%Glam rock%"
-	) sub
+SELECT band_name,
+(IFNULL(split, 2021) - formed) as lifespan
+FROM metal_bands
+WHERE style LIKE "%Glam rock%"
 ORDER BY lifespan DESC;
