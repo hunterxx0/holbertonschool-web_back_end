6f994231af922c282b7dcdf5aada33bddb4f34c8
hunterxx0 <azouzmhamed@gmail.com>
2021-09-14 22:03:10 +0100

    first

---
+++ 0x0F-ES6_promise/6-final-user.js
@@ -0,0 +1,19 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  const res = [];
+  try {
+    res.push({
+      status: 'fulfilled',
+      value: (await signUpUser(firstName, lastName)),
+    });
+    await uploadPhoto(fileName);
+  } catch (error) {
+    res.push({
+      status: 'rejected',
+      value: error,
+    });
+  }
+  return res;
+}
