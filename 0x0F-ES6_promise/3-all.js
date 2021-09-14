dba9df372a7fc59c84cf1a0b230cd6eeef18739a
hunterxx0 <azouzmhamed@gmail.com>
2021-09-14 13:16:00 +0100

    first

--- 0x0F-ES6_promise/3-all.js
+++ 0x0F-ES6_promise/3-all.js
@@ -1,5 +1,9 @@
-export default function handleResponseFromAPI(promise) {
-  return promise.then(() => ({ status: 200, body: 'success' }))
-    .catch((error) => error)
-    .finally(() => console.log('Got a response from the API'));
+import { createUser, uploadPhoto } from './utils';
+
+export default function handleProfileSignup() {
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((data) => {
+      console.log(`${data[0].body} ${data[1].firstName} ${data[1].lastName}`);
+    })
+    .catch(() => console.log('Signup system offline'));
 }
