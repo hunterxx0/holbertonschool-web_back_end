8004ac86b32622a4140dcad8f7bd9ecdcc270e39
hunterxx0 <azouzmhamed@gmail.com>
2021-09-27 14:15:20 +0100

    first

--- 0x12-Node_JS_basic/5-http.js
+++ 0x12-Node_JS_basic/5-http.js
@@ -3,9 +3,7 @@ const countStudents = require('./3-read_file_async');
 
 const app = http.createServer((req, res) => {
   if (req.url === '/') {
-    res.writeHead(200);
-    res.write('Hello Holberton School!');
-    res.end();
+    res.end('Hello Holberton School!');
   }
   if (req.url === '/students') {
     countStudents(process.argv[2])
