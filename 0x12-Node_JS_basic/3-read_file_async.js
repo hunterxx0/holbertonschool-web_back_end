601c3825a45e89cd1f2656943e985fd16a4204bd
hunterxx0 <azouzmhamed@gmail.com>
2021-09-27 14:01:27 +0100

    first

--- 0x12-Node_JS_basic/3-read_file_async.js
+++ 0x12-Node_JS_basic/3-read_file_async.js
@@ -25,7 +25,7 @@ function countStudents(path) {
         }
       }
       for (const [key, value] of Object.entries(dct)) {
-        console.log(`Number of students in ${key}: ${value.length}. List: ${value.join(', ')}`)
+        console.log(`Number of students in ${key}: ${value.length}. List: ${value.join(', ')}`);
         res += (`Number of students in ${key}: ${value.length}. List: ${value.join(', ')}\n`);
       }
       resolve(res);
