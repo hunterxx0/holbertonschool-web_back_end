a6c5a021c6207b7e08531e2ffb586d3dda268ebf
hunterxx0 <azouzmhamed@gmail.com>
2021-09-27 12:58:52 +0100

    first

--- 0x12-Node_JS_basic/2-read_file.js
+++ 0x12-Node_JS_basic/2-read_file.js
@@ -1,30 +1,31 @@
 const fs = require('fs');
 
 function countStudents(path) {
-  fs.readFile(path, 'utf8', (err, data) => {
-    if (err) {
+  let data;
+  try {
+    data = fs.readFileSync(path);
+  } catch (error) {
       throw Error('Cannot load the database');
-    }
-    const lines = data.split('\n').filter((n) => n);
-    console.log(`Number of students: ${lines.length - 1}`);
-    const dct = {};
-    let key = null;
-    let std = null;
-    for (let line = 1; line < lines.length; line += 1) {
-      std = lines[line].split(',');
-      if (std[3]) {
-        key = dct[std[3]];
-        if (key) {
-          key.push(std[0]);
-        } else {
-          dct[std[3]] = [std[0]];
-        }
+  }
+  const lines = data.toString().split('\n').filter((n) => n);
+  console.log(`Number of students: ${lines.length - 1}`);
+  const dct = {};
+  let key = null;
+  let std = null;
+  for (let line = 1; line < lines.length; line += 1) {
+    std = lines[line].split(',');
+    if (std[3]) {
+      key = dct[std[3]];
+      if (key) {
+        key.push(std[0]);
+      } else {
+        dct[std[3]] = [std[0]];
       }
     }
-    for (const [key, value] of Object.entries(dct)) {
-      console.log(`Number of students in ${key}: ${value.length}. List: ${value.join(', ')}`);
-    }
-  });
+  }
+  for (const [key, value] of Object.entries(dct)) {
+    console.log(`Number of students in ${key}: ${value.length}. List: ${value.join(', ')}`);
+  }
 }
 
 module.exports = countStudents;
