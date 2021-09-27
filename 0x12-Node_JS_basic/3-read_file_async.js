328cbe5fb42a96a4a5ec9715ec0e634b9c1f4579
hunterxx0 <azouzmhamed@gmail.com>
2021-09-27 13:12:04 +0100

    first

---
+++ 0x12-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, (err, data) => {
+      if (err) {
+        throw Error('Cannot load the database');
+      }
+      const lines = data.toString().split('\n').filter((n) => n);
+      console.log(`Number of students: ${lines.length - 1}`);
+      const dct = {};
+      let key = null;
+      let std = null;
+      for (let line = 1; line < lines.length; line += 1) {
+        std = lines[line].split(',');
+        if (std[3]) {
+          key = dct[std[3]];
+          if (key) {
+            key.push(std[0]);
+          } else {
+            dct[std[3]] = [std[0]];
+          }
+        }
+      }
+      for (const [key, value] of Object.entries(dct)) {
+        console.log(`Number of students in ${key}: ${value.length}. List: ${value.join(', ')}`);
+      }
+      resolve();
+    });
+  });
+}
+
+module.exports = countStudents;
